5231997b59955c7cceceecadb1f059f15ce1c15e
yeab14 <yeab149@gmail.com>
2024-07-08 11:45:40 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yeab14/Website_Thumbnail_Generator
+
+go 1.22.5
---
+++ main/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    fmt.Println("Starting server...")
+    
+    // Your server logic here
+    http.HandleFunc("/", handlerFunc)
+    http.ListenAndServe(":8080", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+    // Handle requests
+    fmt.Fprintf(w, "Hello, World!")
+}
