bff028035a808068b51e20d4fed6be4870735dd6
yeab14 <yeab149@gmail.com>
2024-07-09 11:10:12 +0300

    binding the thumbnail form with the app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ThumbnailForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ThumbnailForm from './components/ThumbnailForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ThumbnailForm
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your styles here */
 </style>
